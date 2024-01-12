f2c08cf5a81c2223a8592fcc594f126c1c523f78
pepija-garzeke <144416845+pepija-garzeke@users.noreply.github.com>
2024-01-12 14:34:57 +0200

    Update uzd3.py
    TWESTS312

--- uzd3.py
+++ uzd3.py
@@ -9,3 +9,15 @@ a=6
 b=2
 rez2=funkcija(a,b)
 print("Funkcijas rezultāts:",rez2)
+
+def funkcija(x,y):
+    z=2*(x+y)
+    return z
+
+a=7
+rez=funkcija(a,2+a)
+print("Funkcijas rezultāts:",rez)
+a=6
+b=2
+rez2=funkcija(a,b)
+print("Funkcijas rezultāts:",rez2)
